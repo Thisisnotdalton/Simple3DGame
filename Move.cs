19b19d9003e92aebdf23f2ce858db74e21cdacd0
Dalton Nickerson <thisisnotdalton@gmail.com>
2017-11-06 20:44:56 -0500

    Using rigidbody to move.

--- Move.cs
+++ Move.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
     public float speed = 5;
+    //Keep a reference to our rigidbody
+    private Rigidbody rb;
+
 	void Start () { //called when object is created
+        rb = GetComponent<Rigidbody>(); //finds the rigidbody component in this object
 	}
 	
 	void Update () {//called every frame of the game
-        transform.position += new Vector3(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
-        transform.position += new Vector3(0, speed * Input.GetAxis("Vertical") * Time.deltaTime, 0);
+        rb.MovePosition(transform.position + 
+                        new Vector3(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0) +
+                        new Vector3(0, speed * Input.GetAxis("Vertical") * Time.deltaTime, 0));
+
     }
     
 }
