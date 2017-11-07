5e05d6c9b3f14cd64ac063f35fa75bb4d956bb66
Dalton Nickerson <thisisnotdalton@gmail.com>
2017-11-06 20:30:12 -0500

    Using Input.GetAxis.

--- Move.cs
+++ Move.cs
@@ -8,10 +8,6 @@ public class Move : MonoBehaviour {
     //(default) private - only this object can see it
     //protected - only this object and child objects can see it
     public float speed = 5;
-    private bool moveRight = false;
-    private bool moveLeft = false;
-    private bool moveUp = false;
-    private bool moveDown = false;
 	void Start () { //called when object is created
 	}
 	
@@ -20,32 +16,10 @@ public class Move : MonoBehaviour {
         //Vector3 - a set of 3 points (x,y,z)
         //Vector3 newPosition = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
         Vector3 newPosition = transform.position;
-        moveLeft = Input.GetKey(KeyCode.A);
-        moveRight = Input.GetKey(KeyCode.D);
-        moveUp = Input.GetKey(KeyCode.W);
-        moveDown = Input.GetKey(KeyCode.S);
 
-        if (moveRight)
-        {
-            //newPosition = newPosition + new Vector3(speed * Time.deltaTime, 0, 0);
-            // = assignment operator
-            // + addition operator
-            // += increment operator
-            newPosition += new Vector3(speed * Time.deltaTime, 0, 0);
-        }
-        if (moveLeft)
-        {
-            // -= decrement operator
-            newPosition -= new Vector3(speed * Time.deltaTime, 0, 0);
-        }
-        if (moveUp)
-        {
-            newPosition += new Vector3(0, speed * Time.deltaTime, 0);
-        }
-        if (moveDown)
-        {
-            newPosition -= new Vector3(0, speed * Time.deltaTime, 0);
-        }
+
+        newPosition += new Vector3(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+        newPosition += new Vector3(0, speed * Input.GetAxis("Vertical") * Time.deltaTime, 0);
 
         transform.position = newPosition;
 	}
